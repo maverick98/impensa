46beba4d3e853be66fe27e831b1e64857dc2bb4f
manosahu <manosahu@manosahu-lap.idc.oracle.com>
2015-01-22 20:06:03 +0530

    first cut commit for hello world

---
+++ src/main/java/org/impensa/ApplicationConfig.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of j-hawk
+ * Copyright (C) 2012-2013 Manoranjan Sahu, All Rights Reserved.
+ *
+ * This software is provided under the GNU GPL Version 2. In particular,
+ *
+ * 1) If you modify a source file, make a comment in it containing your name and the date.
+ * 2) If you distribute a modified version, you must do it under the GPL 2.
+ * 3) Developers are encouraged but not required to notify the j-hawk maintainers at abeautifulmind98@gmail.com
+ * when they make a useful addition. It would be nice if significant contributions could be merged into the main distribution.
+ *
+ * A full copy of the license can be found in gpl.txt or online at
+ * http://www.gnu.org/licenses/gpl.txt
+ *
+ * END_HEADER
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.impensa;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author manosahu
+ */
+@javax.ws.rs.ApplicationPath("")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(org.impensa.HelloResource.class);
+        resources.add(org.impensa.HelloesResource.class);
+    }
+
+}
---
+++ src/main/java/org/impensa/HelloesResource.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.impensa;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author manosahu
+ */
+@Path("/hs")
+public class HelloesResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HelloesResource
+     */
+    public HelloesResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of org.impensa.HelloesResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/json")
+    public String getJson() {
+        //TODO return proper representation object
+        return "{\"hello\":\"world\"}";
+    }
+
+    /**
+     * POST method for creating an instance of HelloResource
+     * @param content representation for the new resource
+     * @return an HTTP response with content of the created resource
+     */
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response postJson(String content) {
+        //TODO
+        return Response.created(context.getAbsolutePath()).build();
+    }
+
+    /**
+     * Sub-resource locator method for {hello}
+     */
+    @Path("{hello}")
+    public HelloResource getHelloResource(@PathParam("hello") String hello) {
+        return HelloResource.getInstance(hello);
+    }
+}
