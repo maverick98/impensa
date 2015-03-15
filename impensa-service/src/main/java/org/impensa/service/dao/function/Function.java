74b446bafd7b86b5fd8bbc5120c2f58d6c08b8c2
manosahu <manosahu@manosahu-lap.idc.oracle.com>
2015-03-15 11:31:37 +0530

    put some doc around it

--- impensa-service/src/main/java/org/impensa/service/dao/function/Function.java
+++ impensa-service/src/main/java/org/impensa/service/dao/function/Function.java
@@ -1,13 +1,11 @@
 /*
- *  Copyleft(BigBang<-->BigCrunch)  Manoranjan Sahu
- *  
+ * This file is part of impensa.
+ * CopyLeft (C) BigBang<->BigCrunch.All Rights are left.
+ *
+ * 1) Modify it if you can understand.
+ * 2) If you distribute a modified version, you must do it at your own risk.
+ *
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.impensa.service.dao.function;
 
 import java.lang.annotation.ElementType;
--- impensa-service/src/main/java/org/impensa/service/dao/function/IFunctionProvider.java
+++ impensa-service/src/main/java/org/impensa/service/dao/function/IFunctionProvider.java
@@ -1,21 +1,18 @@
 /*
- *  Copyleft(BigBang<-->BigCrunch)  Manoranjan Sahu
- *  
+ * This file is part of impensa.
+ * CopyLeft (C) BigBang<->BigCrunch.All Rights are left.
+ *
+ * 1) Modify it if you can understand.
+ * 2) If you distribute a modified version, you must do it at your own risk.
+ *
  */
-
-
 package org.impensa.service.dao.function;
 
 /**
+ * This is a marker internface which helps in retrieving the function providers.
  *
  * @author manosahu
  */
-/**
- * This is a marker internface which helps in retrieving
- * the function providers.
- * 
- */
 public interface IFunctionProvider {
 
-   
 }
