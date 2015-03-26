deb6f53abef5bad5ee49d3a65b37b55f75af9e01
manosahu <manosahu@manosahu-lap.idc.oracle.com>
2015-03-26 10:33:15 +0530

    fixing

--- impensa-service/src/test/java/org/impensa/service/login/LoginServiceNGTest.java
+++ impensa-service/src/test/java/org/impensa/service/login/LoginServiceNGTest.java
@@ -71,47 +71,47 @@ public class LoginServiceNGTest {
     public void login() throws Exception {
 
         UserDMO userDMO = new UserDMO();
-        userDMO.setUserId("Reetika");
+        userDMO.setUserId("Brian");
         userDMO.setAddress("Uptown Village,17th main street,Sherman,Denver");
         userDMO.setAge(12);
         userDMO.setEmail("rs@rs.com");
-        userDMO.setFirstName("Reetika");
+        userDMO.setFirstName("Brian");
         userDMO.setLastName("Sahoo");
         userDMO.setMiddleName("");
         userDMO.setPhone("303-123-4782");
         userDMO.setEncryptedPassword(EncryptionUtil.encrypt("abcd123#"));
         this.getUserDAO().createUser(userDMO);
-        boolean isLoggedIn = this.getLoginService().login("Reetika", "abcd123#");
+        boolean isLoggedIn = this.getLoginService().login("Brian", "abcd123#");
         Assert.assertEquals(true, isLoggedIn);
-        isLoggedIn = this.getLoginService().isLoggedIn("Reetika");
+        isLoggedIn = this.getLoginService().isLoggedIn("Brian");
         Assert.assertEquals(true, isLoggedIn);
-        System.out.println("Reetika logged in "+isLoggedIn);
-        isLoggedIn = this.getLoginService().login("Reetika", "abcd123#");
+        System.out.println("Brian logged in "+isLoggedIn);
+        isLoggedIn = this.getLoginService().login("Brian", "abcd123#");
         Assert.assertEquals(true, isLoggedIn);
-        System.out.println("Reetika logged in again "+isLoggedIn);
+        System.out.println("Brian logged in again "+isLoggedIn);
         
-        this.getLoginService().logout("Reetika");
+        this.getLoginService().logout("Brian");
         
-        isLoggedIn = this.getLoginService().isLoggedIn("Reetika");
+        isLoggedIn = this.getLoginService().isLoggedIn("Brian");
         
-        System.out.println("Reetika must be logged out "+isLoggedIn);
-        System.out.println("Reetika trying wrong password 1st time");
-        isLoggedIn = this.getLoginService().login("Reetika", "abcd123#1");
+        System.out.println("Brian must be logged out "+isLoggedIn);
+        System.out.println("Brian trying wrong password 1st time");
+        isLoggedIn = this.getLoginService().login("Brian", "abcd123#1");
         Assert.assertEquals(false, isLoggedIn);
         
-        System.out.println("Reetika trying wrong password 2nd time");
-        isLoggedIn = this.getLoginService().login("Reetika", "abcd123#11");
+        System.out.println("Brian trying wrong password 2nd time");
+        isLoggedIn = this.getLoginService().login("Brian", "abcd123#11");
         Assert.assertEquals(false, isLoggedIn);
         
-        System.out.println("Reetika trying wrong password 3rd time");
-        isLoggedIn = this.getLoginService().login("Reetika", "abcd123#112");
+        System.out.println("Brian trying wrong password 3rd time");
+        isLoggedIn = this.getLoginService().login("Brian", "abcd123#112");
         Assert.assertEquals(false, isLoggedIn);
         
         
-        System.out.println("Reetika trying wrong password 4th time");
+        System.out.println("Brian trying wrong password 4th time");
         try{
-            isLoggedIn = this.getLoginService().login("Reetika", "abcd123#112");
-            isLoggedIn = this.getLoginService().login("Reetika", "abcd123#");
+            isLoggedIn = this.getLoginService().login("Brian", "abcd123#112");
+            isLoggedIn = this.getLoginService().login("Brian", "abcd123#");
         }catch(LoginException ex){
             System.out.println(ex.getMessage());
         }
